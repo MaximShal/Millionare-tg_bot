4b6cc94c155d8a466c3ecd40089a3c3252bdd56b
MaximSh <baksbanni31@gmail.com>
2023-01-11 15:36:54 +0200

    v 0.3 Alpha

--- millionare.py
+++ millionare.py
@@ -3,8 +3,26 @@ from random import randint, shuffle
 from aiogram import Bot, types
 from aiogram.dispatcher import Dispatcher
 from aiogram.utils import executor
-from aiogram.types import ReplyKeyboardRemove, ReplyKeyboardMarkup, KeyboardButton, InlineKeyboardMarkup, InlineKeyboardButton
+from aiogram.types import ReplyKeyboardMarkup, KeyboardButton, InlineKeyboardMarkup, InlineKeyboardButton
 import os
+import asyncio
+
+
+bot = Bot(token=os.getenv('TOKEN'))
+dp = Dispatcher(bot)
+photo = open('images/main.png', 'rb')
+help_text = " Приветствую тебя в бета тестированни игры как стать миллионером. По скольку проект маленький " \
+            "детали игры могут отличаться от оригинала, оставь свой коментарий на гите, я постараюсь в ближайшее " \
+            "время выпустить обновление.\n На данном етапе имееться рандомный выбор ответов из правильных.\n" \
+            " В скоре будет добавлена позможность выбора помощи зала, друга и 50/50.\n\n"
+button_new = InlineKeyboardButton('Новая игра', callback_data='button_new')
+keyboard_start = InlineKeyboardMarkup().add(button_new)
+button1 = InlineKeyboardButton('Продолжить', callback_data='continue_start')
+button3 = InlineKeyboardButton('Продолжить', callback_data='continue')
+button4 = KeyboardButton('/start')
+kb1 = InlineKeyboardMarkup().add(button1)
+kb3 = InlineKeyboardMarkup().add(button3)
+kb4 = ReplyKeyboardMarkup().add(button4)
 
 
 class Questions:
@@ -12,7 +30,6 @@ class Questions:
         self._questions = self.create_questions()
         self.answers = self.create_answers()
         self.keys = list(self._questions.keys())
-        self.question = None
 
     @staticmethod
     def create_questions():
@@ -51,7 +68,7 @@ class Questions:
     def take_question(self):
         return self._questions[self.keys[0]]
 
-    def take_answer(self):
+    def take_answers(self):
         return self.answers[self.keys[0]]
 
     def check_answer(self, player_answer):
@@ -69,63 +86,75 @@ class Game:
         self.que = Questions()
         self.varriants = ['a', 'b', 'c', 'd']
         self.total = 0
+        self.money = ['0', '100', '200', '300', '500', '1k', '2k', '4k', '8k', '16k', '32k', '64k', '125k', '250k', '500k',
+                      '1000k']
+        self.__game_flag = False
 
-    def check_correct_answer(self):
-        player_answer = input('>')
-        if player_answer in self.varriants:
-            if self.que.check_answer(self.que.take_answer()[self.varriants.index(player_answer)]):
-                self.total += 1
+    def check_correct_answer(self, player_answer):
+        if self.que.check_answer(self.que.take_answers()[self.varriants.index(player_answer)]):
+            self.total += 1
             self.que.next_question()
         else:
-            print('Некорректно указан ответ, попробуйте еще раз...')
-            self.check_correct_answer()
+            self.__game_flag = True
 
-    def start_game(self):
-        for _ in range(15):
-            print(f'\n{self.que.take_question()}')
-            for a, b in zip(self.varriants, self.que.take_answer()):
-                print(a, b)
-            self.check_correct_answer()
-        print(f'Викторина окончена, Ваш результат: {self.total} из 15 правильных ответов.')
+    async def send_question(self, user_id):
+        kb2 = InlineKeyboardMarkup()
+        for a, b, i in zip(self.varriants, self.que.take_answers(), [1, 2, 3, 4]):
+            button2 = InlineKeyboardButton(a + '. ' + b, callback_data=a)
+            if i % 2 == 1:
+                kb2.add(button2)
+            else:
+                kb2.row(button2)
+        await bot.send_message(user_id, self.que.take_question(), reply_markup=kb2)
+
+    async def send_msg_after_check(self, user_id):
+        if self.__game_flag:
+            if self.total >= 5:
+                await bot.send_message(user_id, f'Не в этот раз, но Вы уходите не с пустыми руками, у вас есть незгораемая '
+                                          f'сумма {self.money[self.total-self.total%5]} UAH.',
+                                          reply_markup=kb3)
+            else:
+                await bot.send_message(user_id, 'К моему сожелению Вы проиграли =_(',
+                                          reply_markup=keyboard_start)
+        else:
+            if self.total >= 5:
+                await bot.send_message(user_id, f'Вы выиграли {self.money[self.total]} UAH. У Вас есть незгораемая '
+                                          f'сумма {self.money[self.total-self.total%5]} UAH. Хотите продолжить?',
+                                          reply_markup=kb3)
+            else:
+                await bot.send_message(user_id, f'Вы выиграли {self.money[self.total]} UAH. Хотите продолжить?',
+                                          reply_markup=kb3)
 
 
 def menu():
-    bot = Bot(token=os.getenv('TOKEN'))
-    dp = Dispatcher(bot)
-    photo = open('images/main.png', 'rb')
-    help_text = " Приветствую тебя в бета тестированни игры как стать миллионером. По скольку проект маленький " \
-                "детали игры могут отличаться от оригинала, оставь свой коментарий на гите, я постараюсь в ближайшее " \
-                "время выпустить обновление.\n На данном етапе имееться рандомный выбор ответов из правильных.\n" \
-                " В скоре будет добавлена позможность выбора помощи зала, друга и 50/50, так же вместо тотала " \
-                "правильных ответов, будет отображаться сумма выйграша.\n\n"
-    keyboard = ReplyKeyboardMarkup()
-    button_new = KeyboardButton('Новая игра')
-    keyboard.add(button_new)
-
     @dp.message_handler(commands=['start', 'help'])
     async def command_start(message: types.Message):
-        await message.reply('ok', reply_markup=keyboard)
         await bot.send_photo(message.from_user.id, photo)
-        await bot.send_message(message.from_user.id, help_text)
+        await bot.send_message(message.from_user.id, help_text, reply_markup=keyboard_start)
 
-    @dp.message_handler()
-    async def new_game(message: types.Message):
-        if message.text.lower() == 'новая игра':
-            await message.reply('создаю игру', reply_markup=keyboard.add(KeyboardButton('hihi')))
+    @dp.callback_query_handler(lambda c: c.data == 'button_new')
+    async def callback_kb_main_pre_new(callback_query: types.CallbackQuery):
+        await bot.send_message(callback_query.from_user.id,
+                               'И так, для тебя подготовили 15 вопросов твоя задача дать как можно больше правильных '
+                               'ответов.\n Для продолжения нажми "Продолжить"',
+                               reply_markup=kb1)
+
+    @dp.callback_query_handler(lambda c: c.data == 'continue_start')
+    async def callback_kb_main_new_game(callback_query: types.CallbackQuery):
+        global g
+        g = Game()
+        asyncio.create_task(g.send_question(callback_query.from_user.id))
+
+    @dp.callback_query_handler(lambda c: c.data in ['a', 'b', 'c', 'd'])
+    async def callback_kb_game_answer(callback_query: types.CallbackQuery):
+        g.check_correct_answer(callback_query.data)
+        asyncio.create_task(g.send_msg_after_check(callback_query.from_user.id))
+
+    @dp.callback_query_handler(lambda c: c.data in ['continue'])
+    async def callback_kb_continue(callback_query: types.CallbackQuery):
+        asyncio.create_task(g.send_question(callback_query.from_user.id))
 
     executor.start_polling(dp, skip_updates=True)
-
-    # request1 = input('>')
-    # if request1.lower() == 'new':
-    #     print('\n И так, для тебя подготовили 15 вопросов твоя задача дать как можно больше правильных ответов.\n '
-    #           'для продолжения нажми Enter<-')
-    #     input()
-    #     g = Game()
-    #     g.start_game()
-    # elif request1.lower() == 'q':
-    #     pass
-    # else:
-    #     menu()
 
 
 if __name__ == '__main__':
